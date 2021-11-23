2d5ee7aada8cc7b7dc7a3206f0c35636e2f44c55
Cam <21029087+csqrl@users.noreply.github.com>
2021-11-23 23:12:17 +0000

    Add hooks
    [release]

--- src/init.lua
+++ src/init.lua
@@ -1,3 +1,5 @@
+local Hooks = require(script.Components.Hooks)
+
 return {
   Menu = require(script.Components.Menu),
   MenuAction = require(script.Components.MenuAction),
@@ -6,6 +8,13 @@ return {
   Toolbar = require(script.Components.Toolbar),
   ToolbarButton = require(script.Components.ToolbarButton),
   Widget = require(script.Components.Widget),
+
+  -- Hooks --
+  Hooks = Hooks,
+
+  usePlugin = Hooks.usePlugin,
+  useToolbar = Hooks.useToolbar,
+  useWidget = Hooks.useWidget,
 
   -- Not neccessary to expose, but maybe useful --
   Context = require(script.Components.Context),
