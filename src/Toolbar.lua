56df0d3ba8f6d741a36c2596a6f18eca0d2dde30
Cam <21029087+csqrl@users.noreply.github.com>
2022-10-16 23:27:56 +0100

    Update Toolbar.lua

--- src/Toolbar.lua
+++ src/Toolbar.lua
@@ -1,17 +1,17 @@
 --!strict
 --[=[
-  @class Toolbar
+	@class Toolbar
 
-  ```lua
-  Roact.createElement(Toolbar, {
-    name = "My Toolbar",
-  }, {
-    Button1 = Roact.createElement(Toolbar.Button, {
-      id = "Button1",
-      name = "Button 1",
-    }),
-  })
-  ```
+	```lua
+	Roact.createElement(Toolbar, {
+		name = "My Toolbar",
+	}, {
+		Button1 = Roact.createElement(ToolbarButton, {
+			id = "Button1",
+			name = "Button 1",
+		}),
+	})
+	```
 ]=]
 local Roact = require(script.Parent.Parent.Roact)
 local Context = require(script.Parent.Context)
@@ -21,9 +21,9 @@ local tableUtil = require(script.Parent.Util.tableUtil)
 local Component = Roact.Component:extend("PluginToolbar")
 
 --[=[
-  @interface ToolbarProps
-  @within Toolbar
-  .name string -- The name of the toolbar
+	@interface ToolbarProps
+	@within Toolbar
+	.name string -- The name of the toolbar
 ]=]
 export type ToolbarProps = {
 	name: string,
